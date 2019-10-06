7b31524fd25d1e3b1a8087cfcd309aa5151a5104
League Student <league@iMac-11.attlocal.net>
2019-10-05 17:29:54 -0700

    sdfg
    asdfasd

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -3,25 +3,26 @@ package _01_Olympic_Rings;
 import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-public static void main(String[] args) {
-	Robot timmy = new Robot(400, 700);
-	Robot tammy = new Robot(800, 700);
-	Robot sammy = new Robot(1200, 700);
-	
-	timmy.setSpeed(10);
-	tammy.setSpeed(10);
-	sammy.setSpeed(10);
-	
-	
-	
-	Thread r1 = new Thread(()->timmy.move(400));
-	Thread r2 = new Thread(()->tammy.move(400));
-	Thread r3 = new Thread(()->sammy.move(400));
-	
-	r1.start();
-	r2.start();
-	r3.start();
-}
-}
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot timmy = new Robot(400, 700);
+		Robot tammy = new Robot(800, 700);
+		Robot sammy = new Robot(1200, 700);
 
+		timmy.setSpeed(10);
+		tammy.setSpeed(10);
+		sammy.setSpeed(10);
+		
+		Thread r1 = new Thread(() -> timmy.setX((int) drawX));
+		
+		Thread r11 = new Thread(() -> timmy.setY((int) drawY));
+		Thread r2 = new Thread(() -> tammy.move(400));
+		Thread r3 = new Thread(() -> sammy.move(400));
+
+		r1.start();
+		r11.start();
+		r2.start();
+		r3.start();
+	}
+}
--- src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
+++ src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
@@ -5,6 +5,22 @@ public class ThreadedReverseGreeting {
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
   //but you should structure your program such that the threads print their greetings in reverse order.
-	
+	static int i = 50;
 	/* HINT: You will most likely need to do this with recursion */
+	public static void main (String[] args) {
+		createThread(50);
+	}
+	
+	public static void createThread(int currentThread) {
+		if (currentThread == 0) {
+			System.out.println("Hello from thread" + i);
+		return;
+	}
+		 i = currentThread; 
+		 i--;
+		Thread r3 = new Thread(() -> createThread(i));
+		r3.start();
+		System.out.println("Hello from thread" + i);
+		
+}
 }
