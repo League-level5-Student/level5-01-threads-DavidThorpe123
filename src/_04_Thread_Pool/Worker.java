9e3015202c0b05baf117605532c4d46daf8479ef
League Student <league@iMac-18.local>
2019-10-12 17:26:39 -0700

    b
    b m

--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,10 +1,22 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class Worker implements Runnable {
-
+public class Worker implements Runnable, Task {
+ConcurrentLinkedQueue<Task> taskQueue = new ConcurrentLinkedQueue<>();
+public Worker(ConcurrentLinkedQueue<Task> taskQueue) {
+	taskQueue = new ConcurrentLinkedQueue<>();
+}
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
+		while(taskQueue.isEmpty() == false) {
+			
+			taskQueue.remove();
+		}
+	}
+	@Override
+	public void perform() {
 		// TODO Auto-generated method stub
 		
 	}
